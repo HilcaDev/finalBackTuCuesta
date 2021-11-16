f2009664073b5081d82e13da952c6a148abd83c5
AlexBMacLeod <macale@sas.upenn.edu>
2021-11-16 13:24:11 -0300

    Agregando modelo candidato

---
+++ src/main/java/com/tuCuesta/encuestas/models/CandidatoModel.java
@@ -0,0 +1,30 @@
+package com.tuCuesta.encuestas.models;
+
+import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "candidatos")
+public class CandidatoModel {
+    
+    @Id
+    private String id;
+    private String nombre;
+
+    public String getId(){
+        return id;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+}
